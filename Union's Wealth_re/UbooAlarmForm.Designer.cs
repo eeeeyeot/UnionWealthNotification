af8f02c925d0866c67c280bcdc813ec76d9040c2
EEEEYeot <ghfuf12@naver.co.kr>
2021-02-26 19:18:31 +0900

    init

---
+++ Union's Wealth_re/UbooAlarmForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Union_s_Wealth_re
+{
+	partial class UbooAlarmForm
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UbooAlarmForm));
+			this.mesoStatusLabel = new System.Windows.Forms.Label();
+			this.timeLabel = new System.Windows.Forms.Label();
+			this.timeTextBox = new System.Windows.Forms.TextBox();
+			this.stopUnionButton = new System.Windows.Forms.Button();
+			this.executeUnionButton = new System.Windows.Forms.Button();
+			this.unionProgressBar = new System.Windows.Forms.ProgressBar();
+			this.stopMesoButton = new System.Windows.Forms.Button();
+			this.executeMesoButton = new System.Windows.Forms.Button();
+			this.mesoProgressBar = new System.Windows.Forms.ProgressBar();
+			this.set100secButton = new System.Windows.Forms.Button();
+			this.set80secButton = new System.Windows.Forms.Button();
+			this.unionStatusLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// mesoStatusLabel
+			// 
+			this.mesoStatusLabel.Location = new System.Drawing.Point(98, 330);
+			this.mesoStatusLabel.Name = "mesoStatusLabel";
+			this.mesoStatusLabel.Size = new System.Drawing.Size(160, 23);
+			this.mesoStatusLabel.TabIndex = 0;
+			this.mesoStatusLabel.Text = "mesoStatus";
+			this.mesoStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// timeLabel
+			// 
+			this.timeLabel.Location = new System.Drawing.Point(85, 9);
+			this.timeLabel.Name = "timeLabel";
+			this.timeLabel.Size = new System.Drawing.Size(188, 23);
+			this.timeLabel.TabIndex = 1;
+			this.timeLabel.Text = "시간을 입력하세요 (숫자만)";
+			this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// timeTextBox
+			// 
+			this.timeTextBox.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.timeTextBox.Location = new System.Drawing.Point(130, 35);
+			this.timeTextBox.Name = "timeTextBox";
+			this.timeTextBox.Size = new System.Drawing.Size(100, 35);
+			this.timeTextBox.TabIndex = 2;
+			this.timeTextBox.Text = "30";
+			this.timeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.timeTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.timeTextBox_KeyDown);
+			// 
+			// stopUnionButton
+			// 
+			this.stopUnionButton.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.stopUnionButton.Location = new System.Drawing.Point(87, 76);
+			this.stopUnionButton.Name = "stopUnionButton";
+			this.stopUnionButton.Size = new System.Drawing.Size(90, 70);
+			this.stopUnionButton.TabIndex = 3;
+			this.stopUnionButton.Text = "중지";
+			this.stopUnionButton.UseVisualStyleBackColor = true;
+			this.stopUnionButton.Click += new System.EventHandler(this.stopUnionButton_Click);
+			// 
+			// executeUnionButton
+			// 
+			this.executeUnionButton.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.executeUnionButton.Location = new System.Drawing.Point(183, 76);
+			this.executeUnionButton.Name = "executeUnionButton";
+			this.executeUnionButton.Size = new System.Drawing.Size(90, 70);
+			this.executeUnionButton.TabIndex = 3;
+			this.executeUnionButton.Text = "실행";
+			this.executeUnionButton.UseVisualStyleBackColor = true;
+			this.executeUnionButton.Click += new System.EventHandler(this.executeUnionButton_Click);
+			// 
+			// unionProgressBar
+			// 
+			this.unionProgressBar.Location = new System.Drawing.Point(42, 150);
+			this.unionProgressBar.Name = "unionProgressBar";
+			this.unionProgressBar.Size = new System.Drawing.Size(280, 26);
+			this.unionProgressBar.TabIndex = 4;
+			this.unionProgressBar.Value = 100;
+			// 
+			// stopMesoButton
+			// 
+			this.stopMesoButton.Location = new System.Drawing.Point(102, 239);
+			this.stopMesoButton.Name = "stopMesoButton";
+			this.stopMesoButton.Size = new System.Drawing.Size(75, 58);
+			this.stopMesoButton.TabIndex = 5;
+			this.stopMesoButton.Text = "메소 중지";
+			this.stopMesoButton.UseVisualStyleBackColor = true;
+			this.stopMesoButton.Click += new System.EventHandler(this.stopMesoButton_Click);
+			// 
+			// executeMesoButton
+			// 
+			this.executeMesoButton.Location = new System.Drawing.Point(183, 239);
+			this.executeMesoButton.Name = "executeMesoButton";
+			this.executeMesoButton.Size = new System.Drawing.Size(75, 58);
+			this.executeMesoButton.TabIndex = 5;
+			this.executeMesoButton.Text = "메소 실행";
+			this.executeMesoButton.UseVisualStyleBackColor = true;
+			this.executeMesoButton.Click += new System.EventHandler(this.executeMesoButton_Click);
+			// 
+			// mesoProgressBar
+			// 
+			this.mesoProgressBar.Location = new System.Drawing.Point(87, 316);
+			this.mesoProgressBar.Name = "mesoProgressBar";
+			this.mesoProgressBar.Size = new System.Drawing.Size(186, 11);
+			this.mesoProgressBar.TabIndex = 4;
+			this.mesoProgressBar.Value = 100;
+			// 
+			// set100secButton
+			// 
+			this.set100secButton.Location = new System.Drawing.Point(277, 239);
+			this.set100secButton.Name = "set100secButton";
+			this.set100secButton.Size = new System.Drawing.Size(75, 23);
+			this.set100secButton.TabIndex = 6;
+			this.set100secButton.Text = "1m 40s";
+			this.set100secButton.UseVisualStyleBackColor = true;
+			this.set100secButton.Click += new System.EventHandler(this.set100secButton_Click);
+			// 
+			// set80secButton
+			// 
+			this.set80secButton.Location = new System.Drawing.Point(277, 274);
+			this.set80secButton.Name = "set80secButton";
+			this.set80secButton.Size = new System.Drawing.Size(75, 23);
+			this.set80secButton.TabIndex = 6;
+			this.set80secButton.Text = "1m 20s";
+			this.set80secButton.UseVisualStyleBackColor = true;
+			this.set80secButton.Click += new System.EventHandler(this.set80secButton_Click);
+			// 
+			// unionStatusLabel
+			// 
+			this.unionStatusLabel.Location = new System.Drawing.Point(98, 179);
+			this.unionStatusLabel.Name = "unionStatusLabel";
+			this.unionStatusLabel.Size = new System.Drawing.Size(160, 23);
+			this.unionStatusLabel.TabIndex = 7;
+			this.unionStatusLabel.Text = "unionStatus";
+			this.unionStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// UbooAlarmForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(364, 361);
+			this.Controls.Add(this.unionStatusLabel);
+			this.Controls.Add(this.set80secButton);
+			this.Controls.Add(this.set100secButton);
+			this.Controls.Add(this.executeMesoButton);
+			this.Controls.Add(this.stopMesoButton);
+			this.Controls.Add(this.mesoProgressBar);
+			this.Controls.Add(this.unionProgressBar);
+			this.Controls.Add(this.executeUnionButton);
+			this.Controls.Add(this.stopUnionButton);
+			this.Controls.Add(this.timeTextBox);
+			this.Controls.Add(this.timeLabel);
+			this.Controls.Add(this.mesoStatusLabel);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "UbooAlarmForm";
+			this.Text = "유부 알리미";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label mesoStatusLabel;
+		private System.Windows.Forms.Label timeLabel;
+		private System.Windows.Forms.TextBox timeTextBox;
+		private System.Windows.Forms.Button stopUnionButton;
+		private System.Windows.Forms.Button executeUnionButton;
+		private System.Windows.Forms.ProgressBar unionProgressBar;
+		private System.Windows.Forms.Button stopMesoButton;
+		private System.Windows.Forms.Button executeMesoButton;
+		private System.Windows.Forms.ProgressBar mesoProgressBar;
+		private System.Windows.Forms.Button set100secButton;
+		private System.Windows.Forms.Button set80secButton;
+		private System.Windows.Forms.Label unionStatusLabel;
+	}
+}
---
+++ Union's Wealth_re/UbooAlarmForm.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Union_s_Wealth_re
+{
+	public partial class UbooAlarmForm : Form
+	{
+		private const int SECOND = 60;
+		private const int PER = 100;
+
+		private static Thread uth = null;
+		private static Thread mth = null;
+
+		private SoundPlayer unionPlayer;
+		private SoundPlayer mesoPlayer;
+
+		private Color defaultColor;
+
+		private double mesoTime = 100;
+		private Thread Uth
+		{
+			get
+			{
+				if (uth == null || !uth.IsAlive)
+					uth = new Thread(new ParameterizedThreadStart(Process));
+
+				return uth;
+			}
+			set
+			{
+				uth = value;
+			}
+		}
+
+		private Thread Mth
+		{
+			get
+			{
+				if (mth == null || !mth.IsAlive)
+					mth = new Thread(new ParameterizedThreadStart(Process));
+
+				return mth;
+			}
+			set
+			{
+				mth = value;
+			}
+		}
+
+		public UbooAlarmForm()
+		{
+			InitializeComponent();
+			stopUnionButton.Enabled = false;
+			stopMesoButton.Enabled = false;
+
+			unionPlayer = new SoundPlayer(Union_s_Wealth_re.Properties.Resources.union);
+			mesoPlayer = new SoundPlayer(Union_s_Wealth_re.Properties.Resources.meso);
+
+			defaultColor = this.BackColor;
+		}
+
+		private void executeUnionButton_Click(object sender, EventArgs e)
+		{
+			ExecuteUnion();
+		}
+
+		private void stopUnionButton_Click(object sender, EventArgs e)
+		{
+			StopUnion();
+		}
+
+		private void executeMesoButton_Click(object sender, EventArgs e)
+		{
+			ExecuteMeso();
+		}
+
+		private void stopMesoButton_Click(object sender, EventArgs e)
+		{
+			StopMeso();
+		}
+
+		private void set100secButton_Click(object sender, EventArgs e)
+		{
+			mesoTime = 100;
+			mesoStatusLabel.Text = mesoTime.ToTimeFormat();
+		}
+
+		private void set80secButton_Click(object sender, EventArgs e)
+		{
+			mesoTime = 80;
+			mesoStatusLabel.Text = mesoTime.ToTimeFormat();
+		}
+
+		private void timeTextBox_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+				ExecuteUnion();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (Uth.IsAlive)
+			{
+				Console.WriteLine("Interrupt  for Form Close");
+				Uth.Interrupt();
+
+				Console.WriteLine("Waiting until thread stops  for Form Close");
+				Uth.Join();
+			}
+
+			if(Mth.IsAlive) {
+				Console.WriteLine("Interrupt  for Form Close");
+				Mth.Interrupt();
+
+				Console.WriteLine("Waiting until thread stops  for Form Close");
+				Mth.Join();
+			}
+		}
+
+		private void ExecuteUnion()
+		{
+			Uth.Start(Category.UNION);
+			stopUnionButton.Enabled = true;
+			executeUnionButton.Enabled = false;
+		}
+
+		private void StopUnion()
+		{
+			Console.WriteLine("Interrupting Union Thread");
+			Uth.Interrupt();
+
+			Console.WriteLine("Waiting until Union thread stops");
+			Uth.Join();
+
+			this.BackColor = defaultColor;
+			stopUnionButton.Enabled = false;
+			executeUnionButton.Enabled = true;
+		}
+
+		private void ExecuteMeso()
+		{
+			Mth.Start(Category.MESO);
+			stopMesoButton.Enabled = true;
+			executeMesoButton.Enabled = false;
+			set100secButton.Enabled = false;
+			set80secButton.Enabled = false;
+		}
+
+		private void StopMeso()
+		{
+			Console.WriteLine("Interrupting Meso Thread");
+			Mth.Interrupt();
+
+			Console.WriteLine("Waiting until Meso thread stops");
+			Mth.Join();
+
+			this.BackColor = defaultColor;
+			stopMesoButton.Enabled = false;
+			executeMesoButton.Enabled = true;
+			set100secButton.Enabled = true;
+			set80secButton.Enabled = true;
+		}
+
+		private void Process(object param)
+		{
+			Category category = (Category)param;
+			int count;
+			Thread timeThread = null;
+
+			if (category == Category.UNION)
+				count = (int)(Convert.ToDouble(timeTextBox.Text) * SECOND);
+			else
+				count = (int)mesoTime;
+
+			try
+			{
+				while (true)
+				{
+					if (timeThread == null || !timeThread.IsAlive)
+					{
+						if (category == Category.UNION)
+						{
+							unionProgressBar.Value = PER;
+							if (timeThread != null)
+							{
+								Color tmp = defaultColor;
+								this.BackColor = Color.Red;
+								Thread.Sleep(2000);
+								this.BackColor = tmp;
+							}
+						}
+
+						timeThread = new Thread(new ParameterizedThreadStart(TimeProcess));
+						object[] parameters = { count, category };
+						timeThread.Start(parameters);
+					}
+
+					Thread.Sleep(100);
+				}
+
+			}
+			catch (ThreadInterruptedException e)
+			{
+				Console.WriteLine(e);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+			finally
+			{
+				timeThread.Interrupt();
+				timeThread.Join();
+				Console.WriteLine("Clean resource : Process");
+			}
+		}
+
+		private void TimeProcess(object _param)
+		{
+			object[] param = (object[])_param;
+			int goal = (int)param[0];
+			int count = (int)param[0];
+			Category category = (Category)param[1];
+			try
+			{
+				while (count >= 0)
+				{
+					int sec = count % SECOND;
+					int min = count / SECOND;
+					if (category == Category.UNION)
+						unionStatusLabel.Text = string.Format("{0}분 {1}초", min, sec);
+					else
+						mesoStatusLabel.Text = string.Format("{0}분 {1}초", min, sec);
+
+					Console.WriteLine("cnt : {0}", count);
+					Console.WriteLine("count : {0}, goal : {1}, percent : {2}", count, goal, (int)(((double)count / goal) * PER));
+					try
+					{
+						if (category == Category.UNION)
+							unionProgressBar.Value = (int)(((double)count / goal) * PER);
+						else
+							mesoProgressBar.Value = (int)(((double)count / goal) * PER);
+
+					}
+					catch (InvalidOperationException e)
+					{
+						Console.WriteLine(e);
+					}
+
+					count--;
+					Thread.Sleep(1000);
+				}
+			}
+			catch (ThreadInterruptedException e)
+			{
+				Console.WriteLine(e);
+			}
+			catch(Exception e) {
+				Console.WriteLine(e);
+			}
+			finally
+			{
+				if (count <= 0)
+				{
+					if (category == Category.UNION)
+						unionPlayer.Play();
+					else
+						mesoPlayer.Play();
+				}
+				Console.WriteLine("TimeProcess Closing");
+			}
+		}
+
+		enum Category
+		{
+			UNION,
+			MESO
+		}
+	}
+	static class Util
+	{
+		public static string ToTimeFormat(this double time)
+		{
+			int sec = (int)(time % 60);
+			int min = (int)(time / 60);
+			Console.WriteLine("time : " + time);
+			return string.Format("{0}분 {1}초", min, sec);
+		}
+	}
+}
